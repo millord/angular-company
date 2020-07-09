7e5b3fc52d1032b1c5bac98a1135046768dad491
millord <juancarlosmillord@gmail.com>
2020-07-09 12:49:08 -0400

    before to use it in the new laptop

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnChanges, Input } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'pm-star',
@@ -8,7 +14,12 @@ import { Component, OnChanges, Input } from '@angular/core';
 export class StarComponent implements OnChanges {
   @Input() rating: number;
   starWidth: number;
+  @Output() ratingClicked: EventEmitter<string> = new EventEmitter<string>();
+
   ngOnChanges() {
     this.starWidth = (this.rating * 75) / 5;
+  }
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked`);
   }
 }
