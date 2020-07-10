2248f93ca3883585d76741ab283f19e2d29f1670
Juan Carlos Millord Peguero <jmillord@apap.com.do>
2020-07-10 13:16:52 -0400

    almost done

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'pm-product-detail',
@@ -10,9 +11,28 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Detail Page'
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id')
+
+    this.pageTitle += `${id}`
+    this.product = {
+
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2019",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    }
+
+
+  }
+  onBack(): void {
+    this.router.navigate(['/products'])
   }
 
 }
---
+++ src/app/products/product-detail.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Router } from "@angular/router"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductDetailGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let id = +next.url[1].path
+    if (isNaN || id < 1) {
+      alert('Invalid product id')
+      this.router.navigate(['/products'])
+      return false
+    }
+    return true
+
+  }
+  constructor(private router: Router) {
+
+  }
+
+}
