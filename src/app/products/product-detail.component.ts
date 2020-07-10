de61091ff5a8304990abfa74229c7e1c9bd1989d
Juan Carlos Millord Peguero <jmillord@apap.com.do>
2020-07-10 16:17:20 -0400

    added my stuff

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -10,24 +10,60 @@ import { ActivatedRoute, Router } from '@angular/router'
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Detail Page'
   product: IProduct;
+  imageWidth: number = 300;
+  imageMargin: number = 2
 
   constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     let id = +this.route.snapshot.paramMap.get('id')
 
-    this.pageTitle += `${id}`
-    this.product = {
-
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2019",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "assets/images/leaf_rake.png"
+    this.pageTitle += ` ${id}`
+    this.product =
+    
+    {
+      "productId": 2,
+      "productName": "Garden Cart",
+      "productCode": "GDN-0023",
+      "releaseDate": "March 18, 2019",
+      "description": "15 gallon capacity rolling garden cart",
+      "price": 32.99,
+      "starRating": 4.2,
+      "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2019",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
+    },
+    {
+      "productId": 8,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2019",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "assets/images/saw.png"
+    },
+    {
+      "productId": 10,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2018",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "assets/images/xbox-controller.png"
     }
+
+
+
 
 
   }
--- src/app/products/product-detail.guard.ts
+++ src/app/products/product-detail.guard.ts
@@ -11,7 +11,7 @@ export class ProductDetailGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     let id = +next.url[1].path
-    if (isNaN || id < 1) {
+    if (isNaN(id) || id < 1) {
       alert('Invalid product id')
       this.router.navigate(['/products'])
       return false
